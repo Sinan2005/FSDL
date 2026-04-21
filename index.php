e3544a9d25c411da1784c5359a3e818bfe36a276
Sinan2005 <tamakesinan04@gmail.com>
2026-04-21 22:14:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<form action="insert.php" method="POST">
+    Name: <input type="text" name="name"><br><br>
+    Email: <input type="text" name="email"><br><br>
+    Mobile: <input type="text" name="mobile"><br><br>
+    <input type="submit" value="Submit">
+</form>
